776d9e7688587a2c520aa1dc4b81e6cd63e5be40
你的名字或ID <>
2025-12-19 15:49:50 +0900

    feat: homework camera UI and practice page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,16 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import HomeworkPage from "./pages/HomeworkPage";
 import PracticePage from "./pages/PracticePage";
 import HomeworkCameraPage from "./pages/HomeworkCameraPage";
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomeworkPage />} />
-        <Route path="/practice" element={<PracticePage />} />
-        <Route path="/camera" element={<HomeworkCameraPage />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<HomeworkPage />} />
+      <Route path="/practice" element={<PracticePage />} />
+      <Route path="/camera" element={<HomeworkCameraPage />} />
+    </Routes>
   );
 };
 
